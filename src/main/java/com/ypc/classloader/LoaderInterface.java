0180b0fa1df302db98436a5fcd75b1a64767d86d
ypc <siniankxq@163.com>
2023-05-23 23:15:00 +0800

    init

---
+++ src/main/java/com/ypc/classloader/LoaderInterface.java
@@ -0,0 +1,12 @@
+package com.ypc.classloader;
+
+/**
+ * @author xun (siniankxq@163.com)
+ * @version 1.0.0
+ * @ClassName LoaderInterface.java
+ * @Description
+ * @createTime 2023年05月23日 21:22:00
+ */
+public interface LoaderInterface {
+	String load(String name);
+}
